a032e918540a7ac03034393065b9004b7bfa11cf
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-16 17:33:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return ;//return decodedArr
+   let decodedArr = []; 
+   for(let i=0; i<encodedStr.length; i++){
+    if(encodedStr[i]==' ')
+        decodedArr.push(' ');
+    else
+        decodedArr.push( lookup[encodedStr[i]] );
+   }
+   return decodedArr.join("");
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
-module.exports = rot13;
+module.exports = rot13; 
